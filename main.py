179fca8937c96fe964160afcb994251cfb475ecf
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-09-12 14:38:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 st.header('Python Compiler') 
 
@@ -10,10 +11,14 @@ button_click2 = st.button('Run multiline code')
 
 if button_click == True:
     with st.container():
-        text = exec(python_input)
+        os.system('touch python1.py')
+        os.system(f'echo {python_input} > python1.py')
+        text = os.popen('python python1.py').read()
         st.markdown(text)
 
 if button_click2 == True:
     with st.container():
-        text = exec(python_input2)
+        os.system('touch python2.py')
+        os.system(f'echo {python_input2} > python2.py')
+        text = os.popen('python python2.py').read()
         st.markdown(text)
